0b2fe8f6d4ce2036da42f1dd6c4424794ca5b812
carmi <carmi@CERMINA_MAGED>
2025-02-10 12:19:08 +0200

    Q01 Element Operator

---
+++ Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using static Assignment.ListGenerators;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Element Operator 
+            #region Q01
+
+           /// 1.Get first Product out of Stock
+           /// 
+
+          /// var Result =ProductList.FirstOrDefault(P=>P.UnitsInStock == 0);
+          ///
+          ///     Result =(from p in ProductList 
+          ///             where p.UnitsInStock == 0
+          ///             select p).FirstOrDefault();
+          ///
+          /// Console.WriteLine(Result);
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
