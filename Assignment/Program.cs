ba25e3fc470a8c1f6eab06ea7b36658977e70420
carmi <carmi@CERMINA_MAGED>
2025-02-10 12:29:14 +0200

    Q02 ElementOperator

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using static Assignment.ListGenerators;
 
 namespace Assignment
@@ -10,20 +11,37 @@ namespace Assignment
             #region Element Operator 
             #region Q01
 
-           /// 1.Get first Product out of Stock
-           /// 
+            /// 1.Get first Product out of Stock
+            /// 
 
-          /// var Result =ProductList.FirstOrDefault(P=>P.UnitsInStock == 0);
-          ///
-          ///     Result =(from p in ProductList 
-          ///             where p.UnitsInStock == 0
-          ///             select p).FirstOrDefault();
-          ///
-          /// Console.WriteLine(Result);
+            /// var Result =ProductList.FirstOrDefault(P=>P.UnitsInStock == 0);
+            ///
+            ///     Result =(from p in ProductList 
+            ///             where p.UnitsInStock == 0
+            ///             select p).FirstOrDefault();
+            ///
+            /// Console.WriteLine(Result);
+
+            #endregion
+
+            #region Q02
+
+            ///  2.Return the first product whose
+            /// Price > 1000, unless there is no match, in which case null is returned.
+            /// 
+
+         ///   var Result = ProductList.FirstOrDefault(P => P.UnitPrice > 1000);
+         ///
+         ///   Result = (from p in ProductList
+         ///           where p.UnitPrice>1000
+         ///           select p).FirstOrDefault();
+         ///   Console.WriteLine(Result);
 
             #endregion
 
-            #endregion
+
+
+                #endregion
         }
-    }
+        }
 }
